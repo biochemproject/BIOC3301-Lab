54e0db35218f94170211dd3de13ae0ac7afdc5d9
biochem3301 <32746890+biochemproject@users.noreply.github.com>
2018-05-08 15:06:45 +0100

    Create make_otu_heatmap.py

---
+++ make_otu_heatmap.py
@@ -0,0 +1,23 @@
+#!/bin/bash --login
+#PBS -l walltime=01:00:00
+#PBS -l select=1:ncpus=2
+#PBS -N 2017_cr_nojoin_no_golay_serial
+#PBS -A d411-training
+
+cd $PBS_O_WORqKDIR
+
+module load miniconda/python2
+
+# setting temporary directory
+mkdir -p ~/qiime_tmp
+export TMPDIR=~/qiime_tmp
+
+# loading virtualenv
+echo "loading virtualenv" 
+
+source activate qiime1
+
+# make OTU heatmap
+make_otu_heatmap.py -i ~/2018_02_smb/SILVA_otus/otu_table.biom -o ~/2018_02_smb/heatmap.pdf
+
+source deactivate
